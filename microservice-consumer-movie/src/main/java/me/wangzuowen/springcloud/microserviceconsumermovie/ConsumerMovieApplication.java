4e72cfc088f0b9a194815ed50866c08901e4689c
Zuowen Wang <wang.zuowen@me.com>
2019-03-24 19:20:08 +0800

    feat: eureka provider consumer

---
+++ microservice-consumer-movie/src/main/java/me/wangzuowen/springcloud/microserviceconsumermovie/ConsumerMovieApplication.java
@@ -0,0 +1,20 @@
+package me.wangzuowen.springcloud.microserviceconsumermovie;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConsumerMovieApplication {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerMovieApplication.class, args);
+    }
+}
