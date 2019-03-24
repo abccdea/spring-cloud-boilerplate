8faa30f02da0cc26b0c640ab4930bc11e5850b41
Zuowen Wang <wang.zuowen@me.com>
2019-03-24 19:54:03 +0800

    feat: ribbon

--- microservice-consumer-movie/src/main/java/me/wangzuowen/springcloud/microserviceconsumermovie/ConsumerMovieApplication.java
+++ microservice-consumer-movie/src/main/java/me/wangzuowen/springcloud/microserviceconsumermovie/ConsumerMovieApplication.java
@@ -2,6 +2,7 @@ package me.wangzuowen.springcloud.microserviceconsumermovie;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class ConsumerMovieApplication {
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
